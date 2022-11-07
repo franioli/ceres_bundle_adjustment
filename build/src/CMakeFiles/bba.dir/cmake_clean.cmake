e5b80952023bc446a161837c2a3ce09e005c12d6
franioli <francesco.ioli@polimi.it>
2022-11-07 21:48:15 +0100

    simple bundle adjustment

---
+++ build/src/CMakeFiles/bba.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bba.dir/simple_bundle_adjuster.cpp.o"
+  "CMakeFiles/bba.dir/simple_bundle_adjuster.cpp.o.d"
+  "bba"
+  "bba.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/bba.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bba.
+# This may be replaced when dependencies are built.
